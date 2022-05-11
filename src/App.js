0b1fdf684601bc12a3076dde30ae3de1eb2ea937
saiteja-gatadi1996 <saitejagatadi543@gmail.com>
2022-05-12 01:06:00 +0530

    notes-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import NotesList from './components/NotesList'
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [notes, setNotes] = useState([
+    { id: nanoid(), text: 'First Note', date: '12/05/2022' },
+    { id: nanoid(), text: 'Second Note', date: '13/05/2022' },
+    { id: nanoid(), text: 'Third Note', date: '14/05/2022' },
+    { id: nanoid(), text: 'Fourth Note', date: '15/05/2022' },
+    { id: nanoid(), text: 'Fifth Note', date: '16/05/2022' },
+    { id: nanoid(), text: 'Sixth Note', date: '17/05/2022' },
+  ])
+
+  const addNote = (text) => {
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: new Date().toLocaleDateString(),
+    }
+    const newNotes = [...notes, newNote]
+    setNotes(newNotes)
+  }
+
+  const deleteNote = (id) => {
+    setNotes(notes.filter((note) => note.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <NotesList notes={notes} addNote={addNote} deleteNote={deleteNote} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
